6d52dc7681b5a071f641c6e5500374544b401a2a
kun99 <kuns.km@gmail.com>
2023-11-24 16:28:55 +0700

    scrollable titles

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,26 +7,25 @@ const subreddits = ref([]);
 
 async function fetchSubreddit() {
   var subs = "";
-  followedSubreddits.value.forEach(sub => {
-    subs = subs+sub+","
+  followedSubreddits.value.forEach((sub) => {
+    subs = subs + sub + ",";
   });
-  await axios
-    .get("/subreddits/" + subs)
-    .then((response) => {
-      subreddits.value = response.data;
-      console.log(subreddits.value.subreddits);
-    });
+  await axios.get("/subreddits/" + subs).then((response) => {
+    subreddits.value = response.data;
+    console.log(subreddits.value.subreddits);
+  });
 }
 
-async function deleteSubreddit(subreddit){
+async function deleteSubreddit(subreddit) {
   var newSubreddits = [];
-  followedSubreddits.value.forEach(sub => {
-    if(subreddit!==sub){
+  followedSubreddits.value.forEach((sub) => {
+    if (subreddit !== sub) {
       newSubreddits.push(sub);
     }
   });
   followedSubreddits.value = newSubreddits;
   fetchSubreddit();
+  console.log(followedSubreddits.value);
 }
 
 onBeforeMount(() => {
@@ -40,7 +39,9 @@ onBeforeMount(() => {
     <div v-for="(subreddit, index) in subreddits.subreddits">
       <div class="flex flex-row pb-4">
         <div class="flex justify-center w-1/3"></div>
-        <div class="flex justify-center w-1/3">r/{{followedSubreddits[index]}}</div>
+        <div class="flex justify-center w-1/3 text-xl">
+          r/{{ followedSubreddits[index] }}
+        </div>
         <div class="flex justify-end w-1/3">
           <p
             class="text-stone-400 rounded-md w-5 flex justify-center cursor-pointer"
@@ -55,9 +56,11 @@ onBeforeMount(() => {
           <a
             v-bind:href="submission.url"
             target="_blank"
-            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+            class="h-48 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
           >
-            <p class="mb-2 text-xl font-bold tracking-tight text-gray-900">
+            <p
+              class="h-32 overflow-auto text-ellipsis mb-2 text-xl font-bold tracking-tight text-gray-900"
+            >
               {{ submission.title }}
             </p>
             <p class="font-normal text-gray-700">
